1991db0bb6a7f012217d17555d7d548806e6d3d2
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-31 03:41:02 -0700

    Create automagic_paths.rs
    Signed-off-by: Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>

---
+++ reality_os/src/automagic_paths.rs
@@ -0,0 +1,8 @@
+pub const ORGANIC_CPU_CLI_BIN: &str = "/opt/organic_cpu/bin/organic_cpu_cli";
+pub const ORGANIC_CPU_PROFILES_DIR: &str = "/opt/organic_cpu/profiles";
+
+/// Default profile for the primary augmented citizen.
+pub const ORGANIC_CPU_DEFAULT_PROFILE_ID: &str = "bostrom_primary";
+
+/// Default host ID label for Reality.os.
+pub const REALITY_OS_HOST_ID: &str = "reality.os";
