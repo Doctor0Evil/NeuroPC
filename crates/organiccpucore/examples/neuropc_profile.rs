dce0e874d006a43ac035d3f41a22db2a3b03a3b4
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-25 23:44:57 -0700

    Create neuropc_profile.rs
    Signed-off-by: Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>

---
+++ crates/organiccpucore/examples/neuropc_profile.rs
@@ -0,0 +1,7 @@
+const MY_ENV: OrganicAccessibilityEnvelope = neuroprofile! {
+    did = "did:bostrom:…",
+    ocpu = ".ocpu.aln",
+    ocpu_env = ".ocpuenv.aln",
+    roh_max = 0.30,
+    modalities = ["chat", "ide"],
+};
